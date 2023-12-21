25faf2a42d99085d995a9f23d1f41242ceda35d8
Hüseyin Aslan <394788@cs.ktu.edu.tr>
2023-12-21 16:38:27 +0300

    create team in db vell

--- routers/user.js
+++ routers/user.js
@@ -531,6 +531,129 @@ router.get("/dashboard/user/:username", checkAuth, (req, res) => {
     }
   );
 });
+router.post("/createTeam/:username", (req, res) => {
+  console.log("create team çalıştı");
+  const { teamName, selectedFriends } = req.body;
+
+  console.log("teamName: " + teamName);
+  console.log("selectedFriends: " + selectedFriends.length);
+  const totalFriendsNumber = Array.isArray(selectedFriends)
+    ? selectedFriends.length
+    : 1;
+  const createdByUsername = req.session.userInfo.name;
+
+  let friend1, friend2, friend3, friend4, friend5;
+  console.log("selectedFriends.length: " + selectedFriends.length);
+  console.log("*******************");
+  for (let i = 0; i < 8; i++) {
+    console.log(selectedFriends[i]);
+  }
+  if (totalFriendsNumber > 1) {
+    console.log("ife girdi ---");
+    friend1 = selectedFriends[0];
+    friend2 = selectedFriends[1];
+    friend3 = selectedFriends[2];
+    friend4 = selectedFriends[3];
+    friend5 = selectedFriends[4];
+  } else if (totalFriendsNumber == 1) {
+    console.log("else ife girdi ---");
+    friend1 = selectedFriends;
+  }
+  console.log("friend1: " + friend1);
+  console.log("friend2: " + friend2);
+  console.log("friend3: " + friend3);
+  console.log("friend4: " + friend4);
+  console.log("friend5: " + friend5);
+
+  const createTeamQuery =
+    "INSERT INTO team (createdByUsername, teamName, creationDate, member1Username, member2Username, member3Username, member4Username, member5Username) VALUES (?, ?, NOW(), ?, ?, ?, ?, ?)";
+
+  dbConnection.query(
+    createTeamQuery,
+    [
+      createdByUsername,
+      teamName,
+      friend1 || null,
+      friend2 || null,
+      friend3 || null,
+      friend4 || null,
+      friend5 || null,
+    ],
+    (teamErr, teamResult) => {
+      if (teamErr) {
+        console.error("Team Creation Error: ", teamErr);
+        return res.status(500).send("Internal Server Error");
+      }
+
+      // 3. Başarı durumunda kullanıcıyı takım sayfasına yönlendir
+      res.redirect("/users/team/" + req.session.userInfo.name);
+    }
+  );
+});
+
+router.get("/users/team/:username", checkAuth, (req, res) => {
+  const requestedUsername = req.params.username;
+
+  // Şimdi requestedUsername ile veritabanından ilgili kullanıcıyı sorgulayabilirsiniz.
+  const userQuery = "SELECT * FROM users WHERE username = ?";
+
+  dbConnection.query(userQuery, [requestedUsername], (userErr, userResults) => {
+    if (userErr) {
+      console.error("User Query Error: ", userErr);
+      res.status(500).send("Internal Server Error");
+    } else {
+      if (userResults.length > 0) {
+        // Kullanıcı bulundu, bilgileri userInfo'ye ekle
+        const userInfo = {
+          id: userResults[0].id,
+          username: userResults[0].username,
+          password: userResults[0].password,
+          name: userResults[0].name,
+          surname: userResults[0].surname,
+        };
+
+        // Arkadaşları sorgula
+        const friendsQuery = `
+          SELECT u.name, u.surname
+          FROM users u
+          JOIN friendships f ON u.username = f.user2Username
+          WHERE f.user1Username = ?
+          UNION
+          SELECT u.name, u.surname
+          FROM users u
+          JOIN friendships f ON u.username = f.user1Username
+          WHERE f.user2Username = ?;
+        `;
+
+        dbConnection.query(
+          friendsQuery,
+          [requestedUsername, requestedUsername],
+          (friendsErr, friendsResults) => {
+            if (friendsErr) {
+              console.error("Friends Query Error: ", friendsErr);
+              res.status(500).send("Internal Server Error");
+            } else {
+              const friends = friendsResults.map((friend) => ({
+                name: friend.name,
+                surname: friend.surname,
+                username: friend.username, // Eklenen satır
+              }));
+
+              res.render("users/teamCre", {
+                currentPage: "/teamCre",
+                userInfo,
+                friends,
+              });
+            }
+          }
+        );
+      } else {
+        // Kullanıcı bulunamadı
+        res.status(404).send("Kullanıcı bulunamadı");
+      }
+    }
+  });
+});
 
 router.use("/signIn", (req, res) => {
   res.render(path.join("users/signIn"), { currentPage: "/signIn" });
